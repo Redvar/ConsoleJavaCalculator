160205086b7cec7de9463805f3e176e3976e60fe
Pavel Konoplin <pavel.qip@gmail.com>
2020-10-18 18:09:47 +0300

    add start text.

--- src/App.java
+++ src/App.java
@@ -9,11 +9,9 @@ import java.io.IOException;
 public class App {
 
     public static void main(String[] args) throws IOException {
-        App app = new App();
-
-
             //Запуск приложения
-
+        System.out.println("Данная программа принимает, как римские так и арабские цифры.");
+        System.out.println("При вводе у вас обязательно должны стоять пробелы. Пример: 5 + 3 , VI + III .");
         Calculator.calculator();
             //Выход из приложения
         Error.exitApp();
